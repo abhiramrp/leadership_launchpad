11d42a7b0db59258bdcdf8148a7937cd301a713f
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-06-01 23:35:26 -0500

    edited wallpaper

--- src/components/Wallpaper.tsx
+++ src/components/Wallpaper.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Wallpaper() {
     return(
-        <div className="w-full h-8-">
-            <Image src="/images/forest.jpg" width={4000} height={900} alt="forest" />
-            <h1>Heading</h1>
+        <div className="w-full h-8">
+            <Image src="/images/forest.jpg" width={4000} height={900} alt="forest" layout="responsive" />
+            
 
         </div>
     )
